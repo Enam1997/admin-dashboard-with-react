956444293a55e29c493d273a850efde0ca0ca262
Enam <mdenamahmedchowdhury@gmail.com>
2023-02-11 12:48:06 +0600

    Add router and start working in userlist page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import React from "react";
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import { Topbar } from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Users from "./pages/users/Users";
 
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
